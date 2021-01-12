0d6658ad80de2aaa4a7e06cba011cd82016c8f8c
Joe Cheng <joe@rstudio.com>
2021-01-12 00:58:31 -0800

    Add Q&A sessions

--- create-times.R
+++ create-times.R
@@ -9,6 +9,15 @@ if (!exists("speaker_program_df")) {
   speaker_program_df <- read_sheet(url, sheet = "speaker-program")
 }
 
+url2 <- "https://docs.google.com/spreadsheets/d/19NMt0A9vXzUV5eNhGkUPY3r7dFSsXcgPx4GAnxnU2ds/edit#gid=0"
+if (!exists("host_df")) {
+  host_df <- read_sheet(url2, sheet = "proposed-hosts") %>%
+    rowwise() %>%
+    mutate(hosts = paste(collapse = " and ", na.omit(c(name, `co-host`)))) %>%
+    ungroup() %>%
+    select(block, track, session_topic, hosts)
+}
+
 stopifnot(all(table(speaker_program_df$talk_id) == 2))
 
 df <- speaker_program_df %>%
@@ -17,7 +26,7 @@ df <- speaker_program_df %>%
     is.na(actual_duration) ~ duration * 60,
     TRUE ~ suppressWarnings(period_to_seconds(ms(actual_duration)))
   )) %>%
-  select(talk_id, block, track, actual_duration, order, base_time, duration_secs)
+  select(talk_id, block, track, name, actual_duration, order, base_time, duration_secs)
 
 df2 <- df %>% mutate(sub_block = case_when(
   is.na(order) ~ NA_integer_,
@@ -41,7 +50,7 @@ readr::write_csv(df4, "talk_times.csv")
 
 df5 <- df3 %>%
   group_by(block, track, sub_block) %>%
-  summarise(start = min(precise_start), discuss_start = max(precise_end)) %>%
+  summarise(.groups = "drop", name = paste(name, collapse = ", "), start = min(precise_start), discuss_start = max(precise_end)) %>%
   arrange(block, sub_block, track) %>%
   mutate(discuss_end = case_when(
     is.na(track) ~ lubridate::ceiling_date(discuss_start, unit = "hours") + lubridate::minutes(20),
@@ -49,11 +58,19 @@ df5 <- df3 %>%
   )) %>%
   mutate(discuss_duration = as.double(discuss_end - discuss_start, units = "secs")) %>%
   left_join(session_df, by = c("block", "track")) %>%
+  left_join(host_df, by = c("block", "track")) %>%
   mutate(topic = case_when(
     is.na(track) ~ paste(sep = "/", "Discussion", session),
     TRUE ~ paste(sep = "/", "Discussion", paste("Track", track), session)
   )) %>%
   mutate(title = case_when(
-    is.na(track) ~ "Keynote Q&A",
+    is.na(track) ~ paste0("Q&A: ", name),
     TRUE ~ paste0("Discussion: ", session, " ", sub_block)
-  ))
+  )) %>%
+  mutate(description = case_when(
+    is.na(track) ~ paste0("Join us for audience Q&A with keynote speaker ", name, "."),
+    TRUE ~ paste0("Join ", hosts, " for audience Q&A with the preceding speakers in this session.")
+  )) %>%
+  select(topic, title, description, start_time = discuss_start, duration = discuss_duration)
+
+readr::write_csv(df5, "discussion_sessions.csv")
